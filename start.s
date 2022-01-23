02786edf73ddfdd7e42988b572b051c599d6d950
Sam Alws <sam@samalws.com>
2022-01-22 23:18:54 -0600

    gdt (not entirely sure if tests ensure that this work yet)

--- start.s
+++ start.s
@@ -20,47 +20,128 @@ dd MB_CKSUM
 section .bss
 align 16
 stackBottom:
-resb 4096
+times 4096 db 0
 stackTop:
 
 section .data
+gdtMsg  db "Loading GDT...", BKSLASHN, 0
 welcome db "Welcome to Chuck OS! (now in nasm)", BKSLASHN, 0
-vga_row dd 0
-vga_col dd 0
+
+vgaRow dd 0
+vgaCol dd 0
+
+gdtSpec:
+; ENTRY 0
+dd 0 ; base
+dd 0 ; limit
+db 0 ; type
+; ENTRY 1
+dd 0 ; base
+db 0xCF, 0xFF, 0xFF ; limit
+db 0x9A ; type
+; ENTRY 2
+dd 0 ; base
+db 0xCF, 0xFF, 0xFF ; limit
+db 0x92 ; type
+
+gdt:
+times 8*3 db 0
+gdtEnd:
+
+gdtr:
+dw 0
+dd 0
 
 section .text
 start:
 mov esp, stackTop
+; -----
+call clearScreenStd
+mov eax, gdtMsg
+call printStrStd
+; -----
+call makeGdt
+call setGdt
+; -----
 call kernelMain
+
 hang:
 cli
 hlt
 jmp hang
 
+makeGdt:
+mov eax, gdtSpec
+mov ebx, gdt
+.loop:
+call makeGdtN
+add eax, 8
+add ebx, 8
+cmp ebx, gdtEnd
+je retLbl
+jmp makeGdt.loop
+
+makeGdtN:
+; eax: gdt spec entry
+; ebx: gdt entry
+; clobbers cl
+; ------
+; first encode base
+mov cl, eax[3]
+mov ebx[2], cl
+mov cl, eax[2]
+mov ebx[3], cl
+mov cl, eax[1]
+mov ebx[4], cl
+mov cl, eax[0]
+mov ebx[7], cl
+; then encode limit
+mov cl, eax[6]
+mov ebx[0], cl
+mov cl, eax[5]
+mov ebx[1], cl
+mov cl, eax[4]
+mov ebx[6], cl
+; finally encode type
+mov cl, eax[7]
+mov ebx[5], cl
+; done
+ret
+
+setGdt:
+; copypasted from osdev wiki, I don't really understand rn
+mov eax, 0
+mov ax, ds
+shl eax, 4
+add eax, gdt
+mov [gdtr+2], eax
+mov eax, gdtEnd
+sub eax, gdt
+mov [gdtr], ax
+lgdt [gdtr]
+ret
+
 kernelMain:
-call clearScreenStd
-mov eax, welcome
-call printStrStd
 mov eax, welcome
 call printStrStd
 ret
 
 printStrStd:
 ; eax: string start pos
-; uses global vars vga_row and vga_col
-mov ecx, [vga_row]
-mov edx, [vga_col]
+; uses global vars vgaRow and vgaCol
+mov ecx, [vgaRow]
+mov edx, [vgaCol]
 call printStr
-mov [vga_row], ecx
-mov [vga_col], edx
+mov [vgaRow], ecx
+mov [vgaCol], edx
 ret
 
 clearScreenStd:
-; uses global vars vga_row and vga_col
+; uses global vars vgaRow and vgaCol
 call clearScreen
 mov ecx, 0
-mov [vga_row], ecx
-mov [vga_col], ecx
+mov [vgaRow], ecx
+mov [vgaCol], ecx
 ret
 
 printStr:
